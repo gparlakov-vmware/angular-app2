3df0bb4e81fc7272b254d5cacaf26af4a0cd8761
Manfred Steyer <manfred.steyer@gmx.net>
2021-05-18 16:09:49 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+    module.exports = {
+      output: {
+        publicPath: "auto",
+        uniqueName: "angular2"
+      },
+      optimization: {
+        // Only needed to bypass a temporary bug
+        runtimeChunk: false
+      },
+      plugins: [
+        new ModuleFederationPlugin({
+          
+            // For remotes (please adjust)
+            name: "angular2",
+            library: { type: "var", name: "angular2" },
+            filename: "remoteEntry.js",
+            exposes: {
+                './web-components': './src/bootstrap.ts',
+            },        
+
+            shared: ["@angular/core", "@angular/common", "@angular/router"]
+          })
+      ],
+    };
